1b54e91ed11f3e0dc54f555856a71b6c7879de7b
Peskooov <162593364+Peskooov@users.noreply.github.com>
2024-09-22 19:20:47 +0300

    NP3
    Platform generate

--- Assets/Ultimate Adventure 3D/Scripts/PlatformGenerator.cs
+++ Assets/Ultimate Adventure 3D/Scripts/PlatformGenerator.cs
@@ -4,51 +4,72 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
     public GameObject[] pathPrefabs; // массив префабов пути
-    public Transform start; // начальная точка
+    public GameObject start; // начальная точка
     public Transform finish; // конечная точка
     public int pathWidth; // ширина пути
     public int pathLength; // длина пути
     public int maxTurns; // максимальное количество поворотов на пути
     private List<GameObject> pathList = new List<GameObject>(); // список всех префабов пути
 
+    public int pathCount;
+    public int offset;
     void Start()
     {
-        GeneratePaths();
+        //pathList.Add(start);
+        //GeneratePaths();
+        GeneratePathsNew();
     }
 
-    void GeneratePaths()
+    private void GeneratePathsNew()
     {
-        // генерируем несколько путей
-        for (int i = 0; i < 3; i++)
+        for (int j = 0; j < pathCount; j++)
         {
-            // создаем новый путь
-            GameObject newPath = new GameObject("Path");
-            newPath.transform.SetParent(transform);
-            newPath.transform.position = start.position;
-            Vector3 pathDirection = (finish.position - start.position).normalized;
-            int totalPathLength = pathLength + Random.Range(-5, 5);
-            // следуем по этому пути
-            for (int j = 0; j < totalPathLength; j++)
-            {
-                // выбираем случайный префаб и добавляем его в путь
-                int prefabIndex = Random.Range(0, pathPrefabs.Length);
-                GameObject pathPrefab = Instantiate(pathPrefabs[prefabIndex], newPath.transform);
-                pathPrefab.transform.localPosition = Vector3.forward * j * pathWidth;
-                // поворачиваем путь в случайную сторону
-                if (Random.Range(0, 10) < 8 && j > maxTurns)
-                {
-                    Vector3 oldDirection = pathDirection;
-                    pathDirection = Quaternion.Euler(0, 90, 0) * pathDirection;
-                    if (Physics.Raycast(newPath.transform.position, pathDirection, out RaycastHit hitInfo, pathWidth * 2))
-                    {
-                        pathDirection = oldDirection;
-                        j--;
-                        continue;
-                    }
-                }
+            GameObject pathStart = Instantiate(pathPrefabs[0], start.transform.position + new Vector3(j * (pathWidth + offset), 0, 0), Quaternion.identity);
+            pathList.Add(pathStart);
+
+            for (int i = 0; i < pathLength - 1; i++)
+            { 
+                GameObject path = Instantiate(pathPrefabs[Random.Range(0, pathPrefabs.Length)]);
+                path.transform.position = pathList[pathList.Count - 1].transform.position + new Vector3(pathList[j].transform.position.x, 0, pathWidth + offset);
+           
+                pathList.Add(path);
             }
-            // добавляем путь в общий список
-            pathList.Add(newPath);
         }
     }
+
+    /* void GeneratePaths()
+     {
+         // генерируем несколько путей
+         for (int i = 0; i < 3; i++)
+         {
+             // создаем новый путь
+             GameObject newPath = new GameObject("Path");
+             newPath.transform.SetParent(transform);
+             newPath.transform.position = start.position;
+             Vector3 pathDirection = (finish.position - start.position).normalized;
+            // int totalPathLength = pathLength + Random.Range(-5, 5);
+             // следуем по этому пути
+             for (int j = 0; j < pathLength; j++)
+             {
+                 // выбираем случайный префаб и добавляем его в путь
+
+                 var pathPrefab = Instantiate(pathPrefabs[prefabIndex], newPath.transform);
+                 pathPrefab.transform.localPosition = Vector3.forward * j * pathWidth;
+                 // поворачиваем путь в случайную сторону
+                 if (Random.Range(0, 10) < 8 && j > maxTurns)
+                 {
+                     Vector3 oldDirection = pathDirection;
+                     pathDirection = Quaternion.Euler(0, 90, 0) * pathDirection;
+                     if (Physics.Raycast(newPath.transform.position, pathDirection, out RaycastHit hitInfo, pathWidth * 2))
+                     {
+                         pathDirection = oldDirection;
+                         j--;
+                         continue;
+                     }
+                 }
+             }
+             // добавляем путь в общий список
+             pathList.Add(newPath);
+         }
+     }*/
 }
